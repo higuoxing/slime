97f2a901ba762446c86ba5d92b212e67117df023
Xing Guo <higuoxing@gmail.com>
2023-03-29 23:14:07 +0800

    Add support for running scripts.

--- src/bin/lsi.rs
+++ src/bin/lsi.rs
@@ -1,11 +1,22 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+use lambda::error::Errors;
 use lambda::object;
 use lambda::parser;
 use lambda::vm;
 
+use clap::Parser;
 use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
+use rustyline::DefaultEditor;
 
-fn main() -> Result<()> {
+#[derive(Parser)]
+struct Cli {
+    path: Option<std::path::PathBuf>,
+}
+
+fn repl_loop() -> Result<(), Box<dyn Error>> {
     let mut rl = DefaultEditor::new()?;
     let mut lisp_machine = vm::Machine::new();
 
@@ -28,8 +39,8 @@ fn main() -> Result<()> {
                 }
             }
             Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
+                println!("Interrupt!");
+                println!("Input <C-d> to exit");
             }
             Err(ReadlineError::Eof) => {
                 println!("Happy happy joy joy!");
@@ -43,4 +54,36 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn run_script(script_path: &Path) -> Result<(), Box<dyn Error>> {
+    match script_path.extension() {
+        Some(os_str) => {
+            if os_str != "scm" {
+                return Err(Box::new(Errors::RuntimeException(String::from(
+                    "The input script file should end with .scm",
+                ))));
+            }
+        }
+        None => {
+            return Err(Box::new(Errors::RuntimeException(String::from(
+                "Cannot get the input file's extension",
+            ))));
+        }
+    }
+
+    let script = fs::read_to_string(script_path)?;
+    let mut lisp_machine = vm::Machine::new();
+    let parsed_ast = parser::parse_program(script.as_str())?;
+    lisp_machine.eval(parsed_ast)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+    match args.path {
+        Some(p) => run_script(p.as_path()),
+        None => repl_loop(),
+    }
 }
